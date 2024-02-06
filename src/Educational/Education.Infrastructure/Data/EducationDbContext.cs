1b9b0b246d857e8dabb18f4bf94916391d4b09cc
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-02-06 19:14:03 +0500

    dbContext added

---
+++ src/Educational/Education.Infrastructure/Data/EducationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Education.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Education.Infrastructure.Data
+{
+    public class EducationDbContext : DbContext
+    {
+        public EducationDbContext(DbContextOptions<EducationDbContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<StudentGroup> StudentGroups { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<TeacherSubject> TeachersSubjects { get; set; }
+    }
+}
