ff4b54f9abdc248e193dfe5e3b21e26744c74512
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-02-08 17:49:43 +0500

    Migrations

---
+++ src/Educational/Education.Infrastructure/Migrations/20240208122632_StudentCourseEnumAdded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Education.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class StudentCourseEnumAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Course",
+                table: "Students",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Course",
+                table: "Students");
+        }
+    }
+}
