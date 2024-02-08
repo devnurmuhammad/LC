525818bfa7276aaf0edc7de87e3699ed28f1bad4
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-02-08 17:48:30 +0500

    studentDTO added

---
+++ src/Educational/Education.Application/DTOs/StudentDTO.cs
@@ -0,0 +1,17 @@
+﻿using Education.Domain.Enums.StudentEnums;
+
+namespace Education.Application.DTOs
+{
+    public class StudentDTO
+    {
+        public required string Firstname { get; set; }
+        public required string Middlename { get; set; }
+        public required string Lastname { get; set; }
+        public string? Address { get; set; }
+        public required string Phone { get; set; }
+        public required string Email { get; set; }
+        public bool Status { get; set; }
+        public StudentCourse? Course { get; set; }
+        public string? Comment { get; set; }
+    }
+}
