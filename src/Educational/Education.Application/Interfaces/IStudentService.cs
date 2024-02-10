d6ec22c6364119a2c530b4daf5754e96c46d96ff
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-02-10 17:02:16 +0500

    IStudentService added

---
+++ src/Educational/Education.Application/Interfaces/IStudentService.cs
@@ -0,0 +1,16 @@
+﻿using Education.Application.DTOs;
+using Education.Application.VIewModels;
+using Education.Domain.Entities;
+
+namespace Education.Application.Interfaces
+{
+    public interface IStudentService
+    {
+        public Task<bool> CreateAsync(StudentDTO student);
+        public Task<IList<StudentViewModel>> GetAllAsync();
+        public Task<bool> UpdateAsync(int id, StudentDTO student);
+        public Task<bool> DeleteAsync(int id);
+        public Task<StudentViewModel> GetStudentById(int id);
+        public Task<long> GetCountAsync(); 
+    }
+}
