bf9a002055071e6023a4fe5ccec78f5d477e4dcf
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-02-13 15:49:28 +0500

    Teacher model done

---
+++ src/Educational/Education.Application/DTOs/TeacherDTO.cs
@@ -0,0 +1,21 @@
+﻿using Education.Domain.Enums;
+using Education.Domain.Enums.TeacherEnums;
+
+namespace Education.Application.DTOs
+{
+    public class TeacherDTO
+    {
+        public required string Firstname { get; set; }
+        public required string Middlename { get; set; }
+        public required string Lastname { get; set; }
+        public TeacherPosition Position { get; set; }
+        public DateTime EmploymentDate { get; set; }
+        public required string Phone { get; set; }
+        public required string Email { get; set; }
+        public string? Address { get; set; }
+        public required string Passport { get; set; }
+        public GenderEnum Gender { get; set; }
+        public required string Specialty { get; set; }
+        public int GroupId { get; set; }
+    }
+}
---
+++ src/Educational/Education.Application/VIewModels/TeacherViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Education.Domain.Entities;
+
+namespace Education.Application.VIewModels
+{
+    public class TeacherViewModel
+    {
+        public int Id { get; set; }
+        public required string Firstname { get; set; }
+        public required string Middlename { get; set; }
+        public required string Lastname { get; set; }
+        public string? Position { get; set; }
+        public DateTime EmploymentDate { get; set; }
+        public required string Phone { get; set; }
+        public required string Email { get; set; }
+        public string? Address { get; set; }
+        public required string Passport { get; set; }
+        public string? Gender { get; set; }
+        public required string Specialty { get; set; }
+
+        public ICollection<TeacherSubject>? Subjects { get; set; }
+    }
+}
