432291da8c06593b7b21b363079f8c7e19a1e734
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-02-14 14:45:36 +0500

    DependencyGroup created

---
+++ src/Educational/Education.Infrastructure/InfrastructureDependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Education.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Education.Infrastructure
+{
+    public static class InfrastructureDependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<EducationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("ConnectionString"), options =>
+                options.EnableRetryOnFailure());
+            });
+
+            return services;
+        }
+    }
+}
--- src/Educational/Education.Web/Program.cs
+++ src/Educational/Education.Web/Program.cs
@@ -1,38 +1,21 @@
-using Education.Application.Interfaces;
-using Education.Application.Repositories;
-using Education.Application.Repository;
-using Education.Application.Services;
-using Education.Infrastructure.Data;
-using Education.Infrastructure.Repositories;
-using Microsoft.EntityFrameworkCore;
+using Education.Infrastructure;
+using Education.Infrastructure.Dependencies;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services
-    .AddScoped<IStudentRepository, StudentRepository>()
-    .AddScoped<IStudentService, StudentService>()
-    .AddScoped<ITeacherRepository, TeacherRepository>()
-    .AddScoped<ITeacherService, TeacherService>();
-
-builder.Services.AddDbContext<EducationDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString"));
-});
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 
 builder.Services.AddControllersWithViews()
     .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
