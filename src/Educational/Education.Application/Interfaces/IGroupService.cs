33de922b7498e71e07fa5f88f1c0fa4ea3015e8f
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-02-15 19:48:32 +0500

    GroupService added

---
+++ src/Educational/Education.Application/Interfaces/IGroupService.cs
@@ -0,0 +1,15 @@
+﻿using Education.Application.DTOs;
+using Education.Application.VIewModels;
+
+namespace Education.Application.Interfaces
+{
+    public interface IGroupService
+    {
+        public Task<bool> CreateGroup(GroupDTO groupDTO);
+        public Task<IList<GroupViewModel>> GetAllGroups();
+        public Task<bool> UpdateGroup(string number, GroupDTO groupDTO);
+        public Task<bool> DeleteGroup(string number);
+        public Task<GroupViewModel> GetGroupByNumber(string number);
+        public Task<long> GetGroupCount();
+    }
+}
