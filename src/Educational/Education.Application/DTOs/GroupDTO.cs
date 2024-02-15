f650179412bf85775095a67e2f0a4dba9f100b67
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-02-15 19:48:13 +0500

    groupDTO created

---
+++ src/Educational/Education.Application/DTOs/GroupDTO.cs
@@ -0,0 +1,13 @@
+﻿using Education.Domain.Enums.GroupEnums;
+
+namespace Education.Application.DTOs
+{
+    public class GroupDTO
+    {
+        public required string Number { get; set; }
+        public DateTime StartedDate { get; set; }
+        public DateTime? FinishedDate { get; set; }
+        public GroupStatus Status { get; set; } = GroupStatus.Inactive;
+        public int RoomId { get; set; }
+    }
+}
