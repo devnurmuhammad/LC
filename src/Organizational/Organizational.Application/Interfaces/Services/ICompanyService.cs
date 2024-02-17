934cd2c42fb100cf4f69f62ddb07ebc806687dd1
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-02-17 19:48:58 +0500

    company service

---
+++ src/Organizational/Organizational.Application/Interfaces/Services/ICompanyService.cs
@@ -0,0 +1,12 @@
+﻿using Organizational.Application.DTOs;
+using Organizational.Application.ViewModels;
+
+namespace Organizational.Application.Interfaces.Services
+{
+    public interface ICompanyService
+    {
+        public Task<bool> CreateCompany(CompanyDTO company);
+        public CompanyViewModel GetCompany();
+        public Task<bool> UpdateCompany(CompanyDTO company);
+    }
+}
---
+++ src/Organizational/Organizational.Application/Services/CompanyService.cs
@@ -0,0 +1,45 @@
+﻿using Organizational.Application.DTOs;
+using Organizational.Application.Interfaces.Repositories;
+using Organizational.Application.Interfaces.Services;
+using Organizational.Application.ViewModels;
+using Organizational.Domain.Entities;
+
+namespace Organizational.Application.Services
+{
+    public class CompanyService : ICompanyService
+    {
+        private readonly ICompanyRepository _companyRepository;
+        public CompanyService(ICompanyRepository companyRepository)
+        {
+            _companyRepository = companyRepository;
+        }
+
+        public Task<bool> CreateCompany(CompanyDTO company)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CompanyViewModel GetCompany()
+        {
+            Company company = _companyRepository.GetCompany();
+            CompanyViewModel result =  new CompanyViewModel()
+            {
+                Name = company.Name,
+                Address = company.Address,
+                Email = company.Email,
+                Phone = company.Phone,
+                Website = company.Website,
+                Id = company.Id,
+                Branches = company.Branches,
+                CEO = company.CEO,
+            };
+
+            return result;
+        }
+
+        public Task<bool> UpdateCompany(CompanyDTO company)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
