f8335cada74a1f83dfffd7d8d0cad88b605acfe3
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-02-17 12:56:18 +0500

    InfrastructureDependencyInjection created

---
+++ src/Organizational/Organizational.Infrastructure/InfrastructureDependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Organizational.Infrastructure.Data;
+
+namespace Organizational.Infrastructure
+{
+    public static class InfrastructureDependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrganizationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("ConnectionString"), options =>
+                options.EnableRetryOnFailure());
+            });
+
+            return services;
+        }
+    }
+}
