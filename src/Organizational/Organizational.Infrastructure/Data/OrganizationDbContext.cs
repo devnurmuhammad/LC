1ab55f2eea2cec29e27020df76c84b6a75d287de
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-02-17 12:55:34 +0500

    organizationalDbContext created

---
+++ src/Organizational/Organizational.Infrastructure/Data/OrganizationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Organizational.Domain.Entities;
+
+namespace Organizational.Infrastructure.Data
+{
+    public class OrganizationDbContext : DbContext
+    {
+        public OrganizationDbContext(DbContextOptions<OrganizationDbContext> options) : base(options) 
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Company> Company { get; set; }
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Outcome> Outcomes { get; set; }
+        public DbSet<EmployeeOutcome> EmployeeOutcomes { get; set; }
+    }
+}
