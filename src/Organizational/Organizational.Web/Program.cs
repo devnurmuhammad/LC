877a1d641613d255e086f70aa08b7be3fd9e0988
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-02-17 19:50:42 +0500

    company controller

--- src/Organizational/Organizational.Web/Program.cs
+++ src/Organizational/Organizational.Web/Program.cs
@@ -1,19 +1,17 @@
 using Organizational.Infrastructure;
+using Organizational.Infrastructure.Dependencies;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddDependencyGroup();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
