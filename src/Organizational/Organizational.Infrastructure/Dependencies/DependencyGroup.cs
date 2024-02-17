515f4416361d66bf26b64205921607b1b822c77a
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-02-17 19:50:10 +0500

    dependency group created

---
+++ src/Organizational/Organizational.Infrastructure/Dependencies/DependencyGroup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Organizational.Application.Interfaces.Repositories;
+using Organizational.Application.Interfaces.Services;
+using Organizational.Application.Services;
+using Organizational.Infrastructure.Repositories;
+
+namespace Organizational.Infrastructure.Dependencies
+{
+    public static class DependencyGroup
+    {
+        public static IServiceCollection AddDependencyGroup(this IServiceCollection services)
+        {
+            services.AddScoped<ICompanyRepository, CompanyRepository>()
+                    .AddScoped<ICompanyService, CompanyService>();
+
+            return services;
+        }
+    }
+}
