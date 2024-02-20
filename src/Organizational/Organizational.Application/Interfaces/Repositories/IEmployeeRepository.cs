a3b0670651dbb5cd6aa5f69eac4e683abd47b10a
devnurmuhammad <nurmuhammad1557@gmail.com>
2024-02-20 14:03:53 +0500

    EmployeeRepo added

--- src/Organizational/Organizational.Application/Interfaces/Repositories/IEmployeeRepository.cs
+++ src/Organizational/Organizational.Application/Interfaces/Repositories/IEmployeeRepository.cs
@@ -6,9 +6,9 @@ namespace Organizational.Application.Interfaces.Repositories
     {
         public Task<bool> CreateEmployee(Employee employee);
         public Task<IList<Employee>> GetAllEmployee();
-        public Task<bool> UpdateEmployee(int id);
-        //public Task<bool> DeleteEmployee(int id);
-        public Task<IList<Employee>> GetEmployeeByName(string name);
+        public Task<bool> UpdateEmployee(Employee employee);
+        public Task<bool> DeleteEmployee(int id);
+        public Task<Employee> GetEmployeeById(int id);
         public Task<int> GetEmployeeCount();
     }
 }
